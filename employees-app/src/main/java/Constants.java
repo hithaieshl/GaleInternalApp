d813a3149b2227e0def74e966fa5faabfceda95d
Yogi Gokani <Yogi Gokani>
2016-02-22 13:21:43 +0530

    login files

---
+++ employees-app/src/main/java/Constants.java
@@ -0,0 +1,9 @@
+
+public interface Constants {
+
+	String DRIVER = "org.postgresql.Driver";
+	String URL = "jdbc:postgresql://localhost:5432/GaleInternalDev";
+	String UID = "postgres";
+	String PWD = "yogi";
+	String SUCCESS = "success";
+}
